0d84b6c830b1a50e7e9204c8bda6301d48670d40
rcescueta <rachel.escueta@gmail.com>
2026-08-24 13:45:57 +0200

    Fix path to soil db

--- run-producer_calibration.py
+++ run-producer_calibration.py
@@ -156,8 +156,7 @@ def run_producer(server={"server": None, "port": None}):
 
     # gdf = gpd.read_file(NUTS3_REGIONS)
 
-    # soil_db_path = paths["path-to-data-dir"] + DATA_SOIL_DB
-    soil_db_path = DATA_SOIL_DB
+    soil_db_path = paths["path-to-data-dir"] + DATA_SOIL_DB
     # subprocess.run(["wget", "-O", soil_db_path, SOIL_DB_URL], check=True)
     #print("Downloaded soil db successfully.")
 
